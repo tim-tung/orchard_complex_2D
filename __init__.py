f5cfea5326191060c886a8427463646883620e1b
tim-tung <102334201+tim-tung@users.noreply.github.com>
2024-10-28 04:16:00 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from jumanji.environments.routing.robot_warehouse.env import RobotWarehouse
+from env import RobotWarehouse
 from jumanji.environments.routing.robot_warehouse.types import Observation, State
